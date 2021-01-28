d2972f47479b4e76fcd7119173da13a3db7e85ca
DESKTOP-HSQGU69\bon_1 <bon_1918456@hotmail.com>
2021-01-28 22:13:55 +0700

    submit

---
+++ GI455_Test1/Assets/Text_Checking.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_Checking : MonoBehaviour
+{
+    public List<string> textPanelList; //สร้าง List ขึ้นมา
+
+    public Text textPanel; //text ใน Panel
+    public Text textResult; //text ใน ข้อความแสดงผล
+    public InputField textInput; //ตัวแปรที่ text Input เข้ามา
+
+    void Start()
+    {
+        textPanelList = new List<string>() { "Hichat", "Pogchamp", "OmegaLUL", "Iwashere", "OMG" };
+        textPanel.text = string.Join("\n", textPanelList);             
+    }
+
+    public void OnClick()
+    {
+        foreach(var item in textPanelList)
+        {
+            if(item == textInput.text)
+            {
+                textResult.text = $" [ <color=green>{textInput.text}</color> ] is found ";
+                return;
+            }
+            else
+            {
+                textResult.text = $" [ <color=red>{textInput.text}</color> ] is not found ";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
+    
+
